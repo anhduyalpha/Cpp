329282c0571fc65a6b2215b498e08fd09f9a8431
AlphaD <duydang0768134698@gmail.com>
2025-12-12 14:58:57 +0700

    rearrange folder structure

---
+++ University/Knowledge/Struct/Assign Struct.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    struct diem {
+        int x;
+        int y;
+    };
+    diem a;
+    a.x = 2;
+    a,y = 4;
+    cout << a.x << " " << a.y;
+}
